1dfe9e3c29162560810d2b71308a1a7d7ebc3c6f
Unknown <68416923+3jm@users.noreply.github.com>
2022-07-22 01:25:43 -0400

    💜

--- money and bounty notifier.lua
+++ money and bounty notifier.lua
@@ -2,24 +2,45 @@ _G.Sendhook = true -- to change this to false, open a new tab in synapse and jus
 _G.HookTimer = 900 -- 15 minutes
 -- just use google's minute's to seconds converter, or hour idc all works the same
 
+local funny_table = {
+    "-- -.-- / -. ..- - ... / .. - -.-. ....",
+    ".... .. / -- --- -- -.-.--",
+    ".-- .... .- - / -.. --- / .. / .--. ..- - / .... . .-. .",
+    "-... .. --. / -... .-.. .- -.-. -.- / -... .- .-.. .-.. ..."
+}
+
 while _G.Sendhook do
     local webhook = "your webhook here"
     local bountytext = game:GetService("Players").LocalPlayer.PlayerGui.AppUI.Buttons.Bounty.Label.Text
     local currentcash = game:GetService("Players").LocalPlayer.PlayerGui.CashGui.Container.Content.ContainerTop.ContainerRight.ContainerCash.TextLabel.Text
+    -- string.sub(bountytext,9)
     local data = {
-        ["content"] = "",
-        ["embeds"] = {
+        content = "",
+        embeds = {
             {
-                ["title"] = "Jailbreak Auto-Rob ".. _G.HookTimer.. " Second Notifier.",
-               ["description"] = "💸 Your current cash: `".. currentcash .."`\n💢 Your current bounty: `".. string.sub(bountytext,9) .."`",
-               ["url"] = "https://www.roblox.com/games/606849621/Jailbreak",
-               ["type"] = "rich",
-               ["color"] = tonumber(0x00ccff),
-               ["footer"] = {
-                ["text"] = "You will receive another ping in " .. _G.HookTimer .. " seconds. | Made by Blind#2665"
+                title = "Jailbreak Auto-Rob ".. _G.HookTimer.. " Second Notifier.",
+                fields = { -- thank to Cynical#0019 for re-design idea
+                    {
+                        ["name"] = "`💸` Current Cash",
+                        ["value"] = "```".. currentcash.. "```",
+                        ["inline"] = true
+                    },
+                    {
+                        ["name"] = "`💢` Current Bounty",
+                        ["value"] = "```".. string.sub(bountytext,9) .. "```",
+                        ["inline"] = true
+                    },
+                },
+                url = "https://www.roblox.com/games/606849621/Jailbreak",
+                color = tonumber(0x00ccff),
+                footer = {
+                    text = "\"We got them same guns they send off to Russia\" - Gucci Mane."
                }
            }
-       }
+       },
+       username = funny_table[math.random( #funny_table )],
+       avatar_url = "https://cdn.discordapp.com/attachments/995764976369279127/999846528468394004/7708-yb-angry2.png?size=4096",
+       attachments = {}
     }
     local newdata = game:GetService("HttpService"):JSONEncode(data)
 
